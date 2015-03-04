e5bda0c18f6be024f3e650825a1141ea39f9a99b
nemonik <github.com@nemonik.com>
2015-03-04 13:14:16 -0500

    tweaked Round-trip example

--- src/main/java/org/mitre/stix/examples/Xml2Object.java
+++ src/main/java/org/mitre/stix/examples/Xml2Object.java
@@ -1,5 +1,10 @@
+/**
+ * Copyright (c) 2015, The MITRE Corporation. All rights reserved.
+ * See LICENSE for complete terms.
+ */
 package org.mitre.stix.examples;
 
+import org.apache.commons.lang.StringUtils;
 import org.mitre.stix.stix_1.STIXPackage;
 
 import java.io.File;
@@ -8,6 +13,18 @@ import java.net.URISyntaxException;
 import java.net.URL;
 import java.util.Scanner;
 
+/**
+ * Round-trip example. This script takes a STIX instance document from XML to
+ * a binding object, then XML string, and then back into an binding object, which 
+ * is then used to generate an XML string.
+ * 
+ * Same as {@link https
+ * ://raw.githubusercontent.com/STIXProject/python-stix/master
+ * /examples/xml2object.py}.
+ * 
+ * @author nemonik (Michael Joseph Walsh <github.com@nemonik.com>)
+ *
+ */
 public class Xml2Object {
 
 	public Xml2Object() {
@@ -26,9 +43,13 @@ public class Xml2Object {
 	
 			STIXPackage stixPackage = STIXPackage.fromXMLString(text);
 			
-			System.out.println(stixPackage.toXMLString());
+			text = stixPackage.toXMLString();
 			
-			STIXPackage stixPackageTwo = STIXPackage.fromXMLString(stixPackage.toXMLString());
+			System.out.println(text);
+			
+			System.out.println(StringUtils.repeat("-", 120));
+			
+			STIXPackage stixPackageTwo = STIXPackage.fromXMLString(text);
 			
 			System.out.println(stixPackageTwo.toXMLString());
 			
