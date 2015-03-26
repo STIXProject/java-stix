62f77d337ca41165ef68524920ba334b4e20c262
nemonik <github.com@nemonik.com>
2015-03-26 14:05:58 -0400

    i lost my mind... fixes to shema retrieval scripts

--- retrieve_schemas.bat
+++ retrieve_schemas.bat
@@ -1,11 +1,11 @@
 echo Retrieving STIX schemas...
 git submodule init
-#git submodule update --force
+git submodule update --force
+cd src\main\resources\schemas\v1.1.1
 git checkout tags/v1.1.1
-cd src\main\resources\schemas
+cd cybox
 
 echo Retrieving CybOX schemas...
 git submodule init
-#git submodule update --force
+git submodule update --force
 git checkout 3442ebe50385d3bd0b3305952b90d296e0a1242c
-exit 0
