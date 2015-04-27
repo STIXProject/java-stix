322d7f60ee42d0d8811fd06353a64c621f67f086
nemonik <github.com@nemonik.com>
2015-04-27 13:07:03 -0400

    debugging travis ci issue

--- buildSrc/src/main/groovy/org/mitre/stix/RetrieveSchemasTask.groovy
+++ buildSrc/src/main/groovy/org/mitre/stix/RetrieveSchemasTask.groovy
@@ -50,7 +50,7 @@ class RetrieveSchemasTask extends DefaultTask {
 
 	@TaskAction
 	def retrieve() {
-		println !project.file("src/main/resources/schemas/v${schemaVersion}").exists()) 
+		println !project.file("src/main/resources/schemas/v${schemaVersion}").exists()
 		println project.file("src/main/resources/schemas/v${schemaVersion}").list().size() == 0
 		println project.file("src/main/resources/schemas/v${schemaVersion}/cybox").list() == null
 		
